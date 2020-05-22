b23133cb76b4df4bbb2739a4625f41df4919ef54
Anastasiia <nastyona221985@gmail.com>
2020-05-23 00:03:49 +0300

    Updates

---
+++ precache-manifest.15306450ddcf5f6ce461254adbd96fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a20d32d85cf1f8692e856424cc9725",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "f18d2371f1d6ac5a633c",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.8c4e547e.chunk.css"
+  },
+  {
+    "revision": "71b33b0e8434352be5c7",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.382182ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.382182ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18d2371f1d6ac5a633c",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.2a65cb47.chunk.js"
+  },
+  {
+    "revision": "c73043600cf8c264972f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.2d9dc3c9.js"
+  }
+]);
